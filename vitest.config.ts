4b710adc83e20de6237bb67c756b405942c0cffd
Minh Khanh Nguyen <minhkhanh2010@hotmail.com>
2025-03-03 22:51:10 +0100

    #4 Remove unecessary files and packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
   test: {
     globals: true, // Enables global variables like describe, it, expect
     environment: "jsdom", // Use jsdom for browser-like environment (if you're testing React)
-    testTransformMode: {
-      web: ["\\.[tj]sx$"], // Enable transformations for TypeScript/JSX files
-    },
   },
 });
