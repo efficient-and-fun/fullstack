ef794d4487d936ff6a8f020a9dc37e0fcb6462aa
Minh Khanh Nguyen <minhkhanh2010@hotmail.com>
2025-03-07 15:17:02 +0100

    #4 fix cypress and vitest conflict

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:5173/')
-    cy.get("button").click()
-    cy.get("button").should("contain", "count is 1")
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("button").click();
+    cy.get("button").should("contain", "count is 1");
+    cy.get("h1").should("contain", "Vite + React");
+  });
+});
