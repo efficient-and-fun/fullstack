8b49ebb4a55811f05c5c02fe9399b8eadf2b5f9e
Minh Khanh <M1nhKh4nh@users.noreply.github.com>
2025-03-18 22:40:11 +0100

    #15 Create GitHub actions (#20)
    * #15 first draft of frontend pipeline
    
    * #15 working pipeline on local environment
    
    * #15 fix yaml file with setting amd64
    
    * #15 use alternative libasound2 library
    
    * #15 first try of docker build and puch pipeline
    
    * #15 test docker login with token
    
    * #15 fix dockerfile path in pipeline
    
    * #15 fix pipeline yaml file
    
    * #15 fix dockerfile pipeline
    
    * #15 fix path in pipeline for docker
    
    * #15 debugging
    
    * #15 debugging root folder
    
    * #15 add checkout command
    
    * #15 test pipeline and automated deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import ApiForm from './assets/ApiForm'
+import ApiForm from "./assets/ApiForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -19,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React (MIR HENDS GSCHAFFT)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -35,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
