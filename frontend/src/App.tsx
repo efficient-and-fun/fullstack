47e48d3a64bc00ff15b375d9bed5ce3135485461
miweinho <weimic01@students.zhaw.ch>
2025-04-11 10:44:47 +0200

    feat-52-add-nav-bar-prototype

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './routes/Home';
+import { Box } from '@mui/material';
 import EventPage from './routes/Event';
 import FriendsPage from './routes/Friends';
 import NotificationPage from './routes/Notification';
 import SettingPage from './routes/Setting';
+import Nav from './components/Nav';
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
   return (
     <>
     <Router>  
+        <Box>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/event" element={<EventPage />} />
@@ -20,6 +23,8 @@ function App() {
           <Route path="/notification" element={<NotificationPage />} />
           <Route path="/setting" element={<SettingPage />} />
         </Routes>
+        <Nav />
+        </Box>
     </Router>
     </>
   );
