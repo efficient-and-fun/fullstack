d660879eb62197aa47db6a898f40ec99aaceee31
hubersas <hubersas@students.zhaw.ch>
2025-04-14 20:15:20 +0200

    Changed colors to final one

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundImage: 'linear-gradient(180deg, #49B759, #A7D558)',
+          backgroundColor: 'white', // Fallback
+          margin: 0,
+        },
+      },
+    },
+  },
+});
+ 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
