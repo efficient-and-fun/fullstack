f67b6a3f981f0e3a819017124071cc7bc8383850
hubersas <hubersas@students.zhaw.ch>
2025-04-15 10:06:00 +0200

    Added e2e tests

---
+++ frontend/cypress/e2e/navigation.cy.ts
@@ -0,0 +1,46 @@
+describe('Navigation', () => {
+    it('should navigate to Event page when clicking the Event link', () => {
+      cy.visit('http://localhost:5173'); // Deine Dev-URL
+  
+      cy.contains('Event').click(); // Link mit Text "Event" anklicken
+  
+      cy.url().should('include', '/event'); // URL sollte sich ändern
+      cy.contains('Event Page'); // Der Inhalt der Event-Seite sollte angezeigt werden
+    });
+
+    it('should navigate to Home page when clicking the Home link', () => {
+        cy.visit('http://localhost:5173'); // Deine Dev-URL
+    
+        cy.contains('Home').click(); // Link mit Text "Home" anklicken
+    
+        cy.url().should('include', '/'); // URL sollte sich ändern
+        cy.contains('Home Page'); // Der Inhalt der Home-Seite sollte angezeigt werden
+      });
+
+    it('should navigate to Event page when clicking the Notification link', () => {
+        cy.visit('http://localhost:5173'); // Deine Dev-URL
+
+        cy.contains('Notification').click(); // Link mit Text "Event" anklicken
+
+        cy.url().should('include', '/notification'); // URL sollte sich ändern
+        cy.contains('Notification Page'); // Der Inhalt der Event-Seite sollte angezeigt werden
+    });
+
+    it('should navigate to Friends page when clicking the Friends link', () => {
+        cy.visit('http://localhost:5173'); // Deine Dev-URL
+    
+        cy.contains('Friends').click(); // Link mit Text "Friends" anklicken
+    
+        cy.url().should('include', '/friends'); // URL sollte sich ändern
+        cy.contains('Friend Page'); // Der Inhalt der Friends-Seite sollte angezeigt werden
+    });
+
+    it('should navigate to Setting page when clicking the Setting link', () => {
+        cy.visit('http://localhost:5173'); // Deine Dev-URL
+
+        cy.contains('Setting').click(); // Link mit Text "Setting" anklicken
+
+        cy.url().should('include', '/setting'); // URL sollte sich ändern
+        cy.contains('Setting Page'); // Der Inhalt der Setting-Seite sollte angezeigt werden
+    });
+  });
