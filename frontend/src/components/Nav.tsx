45cc5c7abe2713af7b60a89adfee680b9220ea70
Sascha Huber <74604940+saschahuberzh@users.noreply.github.com>
2025-04-16 18:11:04 +0200

    56 frontend navbar adjust style (#60)
    * Changed colors to final one
    
    * Merges
    
    * Added e2e tests

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -6,7 +6,7 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import GrassIcon from '@mui/icons-material/Grass';
 import SettingsIcon from '@mui/icons-material/Settings';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 
 
 
@@ -21,32 +21,97 @@ export default function FixedBottomNavigation() {
   };
 
   return (
-    <BottomNavigation sx={{borderTopLeftRadius: 16, borderTopRightRadius: 16, position: 'fixed',bottom: 0, left:0,right:0}} value={value} onChange={handleChange}>
+    <BottomNavigation sx={{borderTopLeftRadius: 16, borderTopRightRadius: 16, position: 'fixed',bottom: 0, left:0,right:0, bgcolor: '#202020',}} value={value} onChange={handleChange}>
       
       <BottomNavigationAction
         label="Event"
         value="event"
-        icon={<GrassIcon />}
+        icon={<GrassIcon  />}
+        sx={{
+          outline: 'none',
+          '&:focus': {
+            outline: 'none',
+          },
+          color: 'white',
+          '&.Mui-selected': {
+            color: '#49B759',
+            '& .MuiBottomNavigationAction-label': {
+              color: '#49B759',
+            },
+          },
+        }}
       />
       <BottomNavigationAction
         label="Home"
         value=""
         icon={<CalendarMonthIcon />}
+        sx={{
+          outline: 'none',
+          '&:focus': {
+            outline: 'none',
+          },
+          color: 'white',
+          '&.Mui-selected': {
+            color: '#49B759',
+            '& .MuiBottomNavigationAction-label': {
+              color: '#49B759',
+            },
+          },
+        }}
       />
       <BottomNavigationAction
         label="Notification"
         value="notification"
         icon={<NotificationsActiveIcon />}
+        sx={{
+          outline: 'none',
+          '&:focus': {
+            outline: 'none',
+          },
+          color: 'white',
+          '&.Mui-selected': {
+            color: '#49B759',
+            '& .MuiBottomNavigationAction-label': {
+              color: '#49B759',
+            },
+          },
+        }}
       />
       <BottomNavigationAction
         label="Friends"
         value="friends"
-        icon={<LocationOnIcon />}
+        icon={<PeopleAltIcon />}
+        sx={{
+          outline: 'none',
+          '&:focus': {
+            outline: 'none',
+          },
+          color: 'white',
+          '&.Mui-selected': {
+            color: '#49B759',
+            '& .MuiBottomNavigationAction-label': {
+              color: '#49B759',
+            },
+          },
+        }}
       />
       <BottomNavigationAction
         label="Setting"
         value="setting"
         icon={<SettingsIcon />}
+        sx={{
+          outline: 'none',
+          '&:focus': {
+            outline: 'none',
+          },
+          color: 'white',
+          '&.Mui-selected': {
+            color: '#49B759',
+            '& .MuiBottomNavigationAction-label': {
+              color: '#49B759',
+            },
+          },
+        }}
       />
     </BottomNavigation>
   );
