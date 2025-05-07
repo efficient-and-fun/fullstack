3367750202053984e407c9eabb9f421ce8b0b0fb
leiselau <leiselau@students.zhaw.ch>
2025-05-07 17:30:37 +0200

    link to create meet up #78

--- frontend/src/components/Home/Header/HomeHeader.tsx
+++ frontend/src/components/Home/Header/HomeHeader.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import './HomeHeader.css';
+import { useNavigate } from "react-router-dom";
 
 const HomeHeader = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="headercontainer">
-        <button
-            className="btn"
-            >
-            +
-        </button>
+      <button
+        className="btn"
+        onClick={() => navigate('/new')}
+      >
+        +
+      </button>
     </div>
   );
 };
