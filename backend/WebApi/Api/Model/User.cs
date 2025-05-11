a93949fd03837a12a20158b42b682ca94edfd4a5
Linda Riesen <rieselin@students.zhaw.ch>
2025-05-11 11:44:33 +0200

    add userdto

--- backend/WebApi/Api/Model/User.cs
+++ backend/WebApi/Api/Model/User.cs
@@ -15,3 +15,12 @@ public class User
     public string ProfilePicturePath { get; set; }
     public string? DietaryRestrictions { get; set; }
 }
+
+public class UserDto
+{
+    [Required, Key]
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string ProfilePicturePath { get; set; }
+}
