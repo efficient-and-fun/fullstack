ae71f5ba0b913a0d04ba2efb64b7a0bf79cbd1f5
louise <anderlou@students.zhaw.ch>
2025-05-13 17:35:26 +0200

    add UserService.cs

---
+++ backend/WebApi/Api/Service/UserService.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Api.Common;
+using WebApi.Model;
+
+namespace WebApi;
+
+public interface IUserService
+{
+    Task<UserResult> AddFriend(string userId);
+}
+
+
+public class UserService
+{
+    private readonly EfDbContext _context;
+
+    public UserService(EfDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserResult> AddFriend(int userId, string newFriend)
+    {
+        var existingFriendRequest = _context.FriendConnection.FirstOrDefault(fc => fc.UserId == userId && fc.Friend.UserName == newFriend);
+
+        if (existingFriendRequest == null)
+        {
+            var friendId = GetUserIdFromUserName(newFriend);
+            if (friendId == -1)
+            {
+                return new UserResult { Success = false, ErrorMessage = "User not found" };
+            }
+            _context.FriendConnection.Add(
+                new FriendConnection()
+                {
+                    UserId = userId,
+                    FriendId = friendId,
+                    HasAcceptedFriendRequest = true
+                    //TODO: this value needs to be adjust if the notifications are implemented
+                }
+            );
+            
+        }
+        
+        await _context.SaveChangesAsync();
+        return new UserResult { Success = true };
+    }
+
+
+    public int GetUserIdFromUserName(string username)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.UserName == username);
+        if (user == null)
+        {
+            return -1;
+        }
+        
+        return user.UserId;
+    }
+}
+
+public class UserResult
+{
+    public bool Success { get; set; }
+    public string ErrorMessage { get; set; }
+}
