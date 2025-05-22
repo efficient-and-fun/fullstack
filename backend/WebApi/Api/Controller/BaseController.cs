2c11ecd8eb2174751e3598306dc0139f3f9c623c
anderlou <126247755+anderlou@users.noreply.github.com>
2025-05-22 15:18:25 +0200

    95 backend add current apis to backend dev  (#113)
    * add userdto
    
    * pluralize api urls to conform to cleancode
    
    * add getUsers method
    
    * add tests for new getUsers method
    
    * add friend connection mapping for DB
    
    * create helper method to get current user from token in authservice
    
    * create getFriends method in usercontroller
    
    * refactor MeetUpController so the user id is from the token and does not need to be in the URL
    
    * update createMeetup method so the Id is created from the backend and does not need to be given by frontend
    
    * Revert "update createMeetup method so the Id is created from the backend and does not need to be given by frontend"
    
    This reverts commit 4c489e72026687a802f35e0ad0b023dc408cd5d8.
    
    * add mapping for friend connection to EfDbContext
    
    * Update MeetUp Create so that Id does not need to be given from frontend and update tests
    
    * Add tests for getFriends method
    
    * refactor to extracte user validation to extern method
    
    * add UserService.cs
    
    * add api to UserController.cs and changed the tests for the new param
    
    * changed setup of the tests #95
    
    * made some changes as discussed in the meeting #95
    
    * #95 added api endpoint and service method to remove friend.
    
    * edit method addFriend and add tests #95
    
    * add tests
    
    * #95 added additional tests for addfriend.
    
    * #95 finished tests for add and remove friend. Rewrote some error messages. Changed tests where necessary accordingly.
    
    * #95 moved test files.
    
    * #95 moved test files...
    
    * #95 fixed problem that caused the user api endpoints not to work.
    
    * #95 implemented change from Copilot
    
    * #95 added fixed binding attribute.
    
    * #95 removed unnecessary comments.
    
    * #95 refactored MeetUpController.cs and UserController.cs namespaces. Removed unnecessary comments in MeetUpController.cs. Moved private methods from top to bottom part in MeetUpController.cs.
    
    * #95 removed more unnecessary parts from the MeetUpController.cs.
    
    * #95 refactored namespaces and made certain properties and fields nullable where necessary.
    
    * #95 more refactoring for namespaces and other minor things.
    
    ---------
    
    Co-authored-by: Linda Riesen <rieselin@students.zhaw.ch>
    Co-authored-by: fiechdus <fiechdus@students.zhaw.ch>

--- backend/WebApi/Api/Controller/BaseController.cs
+++ backend/WebApi/Api/Controller/BaseController.cs
@@ -1,29 +1,20 @@
-using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WebApi.Api.Common;
 
-namespace WebApi;
+namespace WebApi.Api.Controller;
 
 [ApiController]
 public abstract class BaseController : ControllerBase
 {
-    protected readonly ILogger<BaseController> _logger;
-    protected readonly IConfiguration _configuration;
-    
-    protected readonly EfDbContext _context;
+    protected readonly ILogger<BaseController> Logger;
+    protected readonly IConfiguration Configuration;
+
+    protected readonly EfDbContext Context;
 
     protected BaseController(ILogger<BaseController> logger, IConfiguration configuration, EfDbContext context)
     {
-        _logger = logger;
-        _configuration = configuration;
-        _context = context;
-    }
-
-    protected int GetUserId()
-    {
-        var identityClaim = this.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
-        if (identityClaim == null) return -1;
-        return int.Parse(identityClaim.Value);
+        Logger = logger;
+        Configuration = configuration;
+        Context = context;
     }
 }
---
+++ backend/WebApi/Api/Service/UserService.cs
@@ -0,0 +1,127 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Api.Common;
+using WebApi.Api.Model;
+
+namespace WebApi;
+
+public interface IUserService
+{
+    Task<UserResult> AddFriend(int userId, string newFriend);
+    Task<UserResult> RemoveFriend(int userId, string newFriend);
+}
+
+public class UserService : IUserService
+{
+    private readonly EfDbContext _context;
+
+    public UserService(EfDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserResult> AddFriend(int userId, string newFriend)
+    {
+        try
+        {
+            if (_context.Users.FirstOrDefault(u => u.UserId == userId) == null)
+            {
+                return new UserResult { Success = false, ErrorMessage = "User not found" };
+            }
+            
+            var existingFriendRequest = _context.FriendConnection.FirstOrDefault(fc => fc.UserId == userId && fc.Friend.UserName == newFriend);
+            if (existingFriendRequest != null)
+            {
+                return new UserResult { Success = false, ErrorMessage = "Connection already exists" };
+            }
+
+            var friendId = GetUserIdFromUserName(newFriend);
+            if (friendId == -1)
+            {
+                return new UserResult { Success = false, ErrorMessage = "Friend not found" };
+            }
+            
+            if (userId == friendId)
+            {
+                return new UserResult { Success = false, ErrorMessage = "Find real friends" };
+            }
+
+            _context.FriendConnection.Add(
+                new FriendConnection()
+                {
+                    UserId = userId,
+                    FriendId = friendId,
+                    HasAcceptedFriendRequest = true
+                    //TODO: this value needs to be adjust if the notifications are implemented
+                }
+            );
+
+            await _context.SaveChangesAsync();
+            return new UserResult { Success = true };
+        }
+
+        catch (Exception e)
+        {
+            
+            Console.WriteLine($"An error occurred in AddFriend: {e}");
+            
+            return new UserResult { Success = false, ErrorMessage = "An unexpected error occurred. Please try again later." };
+        }
+    }
+
+    public async Task<UserResult> RemoveFriend(int userId, string newFriend)
+    {
+        try
+        {
+            if (_context.Users.FirstOrDefault(u => u.UserId == userId) == null)
+            {
+                return new UserResult { Success = false, ErrorMessage = "User not found" };
+            }
+            
+            var existingFriendRequest = _context.FriendConnection.FirstOrDefault(fc => fc.UserId == userId && fc.Friend.UserName == newFriend);
+            if (existingFriendRequest == null)
+            {
+                return new UserResult { Success = false, ErrorMessage = "Connection does not exist" };
+            }
+
+            var friendId = GetUserIdFromUserName(newFriend);
+            if (friendId == -1)
+            {
+                return new UserResult { Success = false, ErrorMessage = "Friend not found" };
+            }
+            
+            if (userId == friendId)
+            {
+                return new UserResult { Success = false, ErrorMessage = "User cannot have themselves as friend" };
+            }
+
+            _context.FriendConnection.Remove(existingFriendRequest);
+            
+            await _context.SaveChangesAsync();
+            return new UserResult { Success = true };
+        }
+        catch (Exception e)
+        {
+            return new UserResult { Success = false, ErrorMessage = e.Message };
+        }
+    }
+
+
+    public int GetUserIdFromUserName(string username)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.UserName == username);
+        if (user == null)
+        {
+            return -1;
+        }
+
+        return user.UserId;
+    }
+}
+
+public class UserResult
+{
+    public bool Success { get; set; }
+    public string ErrorMessage { get; set; }
+}
