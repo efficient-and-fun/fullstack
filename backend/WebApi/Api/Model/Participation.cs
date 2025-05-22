73974936ec1a79023bb3ac7b9e4d93ba75e662b6
anderlou <126247755+anderlou@users.noreply.github.com>
2025-05-22 15:18:25 +0200

    95 backend add current apis to backend dev  (#113)
    * add userdto
    
    * pluralize api urls to conform to cleancode
    
    * add getUsers method
    
    * add tests for new getUsers method
    
    * add friend connection mapping for DB
    
    * create helper method to get current user from token in authservice
    
    * create getFriends method in usercontroller
    
    * refactor MeetUpController so the user id is from the token and does not need to be in the URL
    
    * update createMeetup method so the Id is created from the backend and does not need to be given by frontend
    
    * Revert "update createMeetup method so the Id is created from the backend and does not need to be given by frontend"
    
    This reverts commit 4c489e72026687a802f35e0ad0b023dc408cd5d8.
    
    * add mapping for friend connection to EfDbContext
    
    * Update MeetUp Create so that Id does not need to be given from frontend and update tests
    
    * Add tests for getFriends method
    
    * refactor to extracte user validation to extern method
    
    * add UserService.cs
    
    * add api to UserController.cs and changed the tests for the new param
    
    * changed setup of the tests #95
    
    * made some changes as discussed in the meeting #95
    
    * #95 added api endpoint and service method to remove friend.
    
    * edit method addFriend and add tests #95
    
    * add tests
    
    * #95 added additional tests for addfriend.
    
    * #95 finished tests for add and remove friend. Rewrote some error messages. Changed tests where necessary accordingly.
    
    * #95 moved test files.
    
    * #95 moved test files...
    
    * #95 fixed problem that caused the user api endpoints not to work.
    
    * #95 implemented change from Copilot
    
    * #95 added fixed binding attribute.
    
    * #95 removed unnecessary comments.
    
    * #95 refactored MeetUpController.cs and UserController.cs namespaces. Removed unnecessary comments in MeetUpController.cs. Moved private methods from top to bottom part in MeetUpController.cs.
    
    * #95 removed more unnecessary parts from the MeetUpController.cs.
    
    * #95 refactored namespaces and made certain properties and fields nullable where necessary.
    
    * #95 more refactoring for namespaces and other minor things.
    
    ---------
    
    Co-authored-by: Linda Riesen <rieselin@students.zhaw.ch>
    Co-authored-by: fiechdus <fiechdus@students.zhaw.ch>

--- backend/WebApi/Api/Model/Participation.cs
+++ backend/WebApi/Api/Model/Participation.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebApi.Model;
+namespace WebApi.Api.Model;
 
 [Table("Participations")]
 public class Participation
