e86d25c913b044d74743cd4d02d1ce6f7264c884
anderlou <126247755+anderlou@users.noreply.github.com>
2025-06-06 15:22:43 +0200

    Merge current state to dev (#131)
    * 95 backend add current apis to backend dev  (#113)
    
    * add userdto
    
    * pluralize api urls to conform to cleancode
    
    * add getUsers method
    
    * add tests for new getUsers method
    
    * add friend connection mapping for DB
    
    * create helper method to get current user from token in authservice
    
    * create getFriends method in usercontroller
    
    * refactor MeetUpController so the user id is from the token and does not need to be in the URL
    
    * update createMeetup method so the Id is created from the backend and does not need to be given by frontend
    
    * Revert "update createMeetup method so the Id is created from the backend and does not need to be given by frontend"
    
    This reverts commit 4c489e72026687a802f35e0ad0b023dc408cd5d8.
    
    * add mapping for friend connection to EfDbContext
    
    * Update MeetUp Create so that Id does not need to be given from frontend and update tests
    
    * Add tests for getFriends method
    
    * refactor to extracte user validation to extern method
    
    * add UserService.cs
    
    * add api to UserController.cs and changed the tests for the new param
    
    * changed setup of the tests #95
    
    * made some changes as discussed in the meeting #95
    
    * #95 added api endpoint and service method to remove friend.
    
    * edit method addFriend and add tests #95
    
    * add tests
    
    * #95 added additional tests for addfriend.
    
    * #95 finished tests for add and remove friend. Rewrote some error messages. Changed tests where necessary accordingly.
    
    * #95 moved test files.
    
    * #95 moved test files...
    
    * #95 fixed problem that caused the user api endpoints not to work.
    
    * #95 implemented change from Copilot
    
    * #95 added fixed binding attribute.
    
    * #95 removed unnecessary comments.
    
    * #95 refactored MeetUpController.cs and UserController.cs namespaces. Removed unnecessary comments in MeetUpController.cs. Moved private methods from top to bottom part in MeetUpController.cs.
    
    * #95 removed more unnecessary parts from the MeetUpController.cs.
    
    * #95 refactored namespaces and made certain properties and fields nullable where necessary.
    
    * #95 more refactoring for namespaces and other minor things.
    
    ---------
    
    Co-authored-by: Linda Riesen <rieselin@students.zhaw.ch>
    Co-authored-by: fiechdus <fiechdus@students.zhaw.ch>
    
    * 121 backend   cleanup meetup (#124)
    
    * add java doc to all public methods
    
    * refactor meetup so validation is in one single method, and user id from token extratction too
    
    * #122 readded the changes that were previously done on a different branch but that mistakenly had the dev branch as a base. (#125)
    
    ---------
    
    Co-authored-by: Linda Riesen <rieselin@students.zhaw.ch>
    Co-authored-by: fiechdus <fiechdus@students.zhaw.ch>
    Co-authored-by: rieselin <116801399+rieselin@users.noreply.github.com>
    Co-authored-by: Dustin Fiechter <50822352+fiechdus@users.noreply.github.com>

--- backend/WebApi/Api/Model/FriendConnection.cs
+++ backend/WebApi/Api/Model/FriendConnection.cs
@@ -1,14 +1,37 @@
 ﻿namespace WebApi.Api.Model;
 
+/// <summary>
+/// Represents a friendship connection between two users, including whether the request has been accepted.
+/// </summary>
 public class FriendConnection
 {
+    /// <summary>
+    /// Unique identifier for the friend connection.
+    /// </summary>
     public int FriendConnectionId { get; set; }
 
+    /// <summary>
+    /// The ID of the user who initiated the friend request.
+    /// </summary>
     public int UserId { get; set; }
+
+    /// <summary>
+    /// Navigation property for the user who initiated the friend request.
+    /// </summary>
     public User User { get; set; }
 
+    /// <summary>
+    /// The ID of the user who is being added as a friend.
+    /// </summary>
     public int FriendId { get; set; }
+
+    /// <summary>
+    /// Navigation property for the user who is being added as a friend.
+    /// </summary>
     public User Friend { get; set; }
 
+    /// <summary>
+    /// Indicates whether the friend request has been accepted.
+    /// </summary>
     public bool HasAcceptedFriendRequest { get; set; }
 }
